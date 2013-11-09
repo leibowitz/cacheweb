e852992cc75f7d3b2e0711e237d3e11967249cb1
Gianni Moschini <gianni.moschini+git@gmail.com>
2013-11-09 13:50:27 +0000

    Add caching to redis

--- server.js
+++ server.js
@@ -1,31 +1,73 @@
 var http = require('http');
 var url  = require('url');
+var redis  = require('redis'),
+    client = redis.createClient();
+
+function computeKey(host, path, method) {
+    return method + ':' + host + ':' + path;
+}
+
+function makeKeyField(key, prefix) {
+    return prefix + ':' + key;
+}
+
+function getHeadersKey(key) {
+    return makeKeyField(key, 'headers');
+}
+
+function getBodyKey(key) {
+    return makeKeyField(key, 'body');
+}
 
 
 http.createServer(function (req, res) {
 
   var host = req.headers['host'];
-  delete req.headers['host'];
 
-  var proxyRequest = http.request({
-    'method': req.method,
-    'hostname': host,
-    'headers': req.headers,
-    'path': req.url
-  }, function(proxyResponse) {
+  var cacheKey = computeKey(host, req.url, req.method);
 
-    delete proxyResponse.headers['content-length'];
-    res.writeHead(proxyResponse.statusCode, proxyResponse.headers);
-    proxyResponse.setEncoding('utf8');
-    proxyResponse.on('data', function (chunk) {
-        res.write(chunk);
-    });
-    proxyResponse.on('end', function(){
+  client.hgetall(getHeadersKey(cacheKey), function(err, results) {
+    
+    if( results !== null ) {
+        res.writeHead(results['statusCode'], JSON.parse(results['headers']));
+        client.get(getBodyKey(cacheKey), function(err, reply) {
+            res.write(reply);
+        });
         res.end();
-    });
-  }
-  );
-  proxyRequest.end();
+    } else {
+
+      delete req.headers['host'];
+
+      var proxyRequest = http.request({
+        'method': req.method,
+        'hostname': host,
+        'headers': req.headers,
+        'path': req.url
+      }, function(proxyResponse) {
+
+        delete proxyResponse.headers['content-length'];
+
+        res.writeHead(proxyResponse.statusCode, proxyResponse.headers);
+
+        client.hset(getHeadersKey(cacheKey), 'statusCode', proxyResponse.statusCode);
+        client.hset(getHeadersKey(cacheKey), 'headers', JSON.stringify(proxyResponse.headers));
+
+        proxyResponse.setEncoding('utf8');
+        proxyResponse.on('data', function (chunk) {
+            res.write(chunk);
+            client.append(getBodyKey(cacheKey), chunk);
+        });
+
+        proxyResponse.on('end', function(){
+            res.end();
+        });
+      });
+      proxyRequest.end();
+    }
+
+  });
+
+
 }).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
 
