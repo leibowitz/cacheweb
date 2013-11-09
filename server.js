22cc1ec354d2d7776d6e6103aad60bf118abdf14
Gianni Moschini <gianni.moschini+git@gmail.com>
2013-11-09 01:16:59 +0000

    Use host headers to proxy request

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 var http = require('http');
+var url  = require('url');
 
 
 http.createServer(function (req, res) {
-  var proxyRequest = http.request({
-    'hostname': 'www.google.com'
 
+  var host = req.headers['host'];
+  delete req.headers['host'];
+
+  var proxyRequest = http.request({
+    'method': req.method,
+    'hostname': host,
+    'headers': req.headers,
+    'path': req.url
   }, function(proxyResponse) {
 
     delete proxyResponse.headers['content-length'];
