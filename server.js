1ecd65f855fe60608223496357b061942c7d574c
Gianni Moschini <gianni.moschini+git@gmail.com>
2013-11-09 23:15:55 +0000

    Read headers to set TTL on key

--- server.js
+++ server.js
@@ -19,6 +19,31 @@ function getBodyKey(key) {
     return makeKeyField(key, 'body');
 }
 
+function canStore(headers) {
+    var params = headers['cache-control'].split(',');
+    var seconds = false;
+    for(idx in params) {
+        if( params[idx].indexOf('private') !== -1 ) {
+            return false;
+        }
+        if( params[idx].indexOf('no-cache') !== -1 ) {
+            return false;
+        }
+        if( params[idx].indexOf('no-store') !== -1 ) {
+            return false;
+        }
+        if( params[idx].indexOf('=') !== -1 ) {
+            var parts = params[idx].split('=');
+            if( parts[0] == 'max-age' ) {
+                seconds = parts[1];
+            }
+            else if( parts[0] == 's-maxage' ) {
+                seconds = parts[1];
+            }
+        }
+    }
+    return seconds;
+}
 
 http.createServer(function (req, res) {
 
@@ -48,14 +73,26 @@ http.createServer(function (req, res) {
         delete proxyResponse.headers['content-length'];
 
         res.writeHead(proxyResponse.statusCode, proxyResponse.headers);
+        
+        var cacheIt = canStore(proxyResponse.headers);
+        
+        if( cacheIt ) {
+            var headersKey = getHeadersKey(cacheKey);
+            var bodyKey = getBodyKey(cacheKey);
 
-        client.hset(getHeadersKey(cacheKey), 'statusCode', proxyResponse.statusCode);
-        client.hset(getHeadersKey(cacheKey), 'headers', JSON.stringify(proxyResponse.headers));
+            client.hset(headersKey, 'statusCode', proxyResponse.statusCode);
+            client.hset(headersKey, 'headers', JSON.stringify(proxyResponse.headers));
+            client.expire(keadersKey, cacheIt);
+
+            client.del(bodyKey);
+        }
 
         proxyResponse.setEncoding('utf8');
         proxyResponse.on('data', function (chunk) {
             res.write(chunk);
-            client.append(getBodyKey(cacheKey), chunk);
+            if( cacheIt ) {
+                client.append(bodyKey, chunk);
+            }
         });
 
         proxyResponse.on('end', function(){
